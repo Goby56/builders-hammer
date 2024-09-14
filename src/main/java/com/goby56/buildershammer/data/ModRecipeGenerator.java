9991d854c69155ea04a086208da67df58c159c07
Goby56 <60710855+Goby56@users.noreply.github.com>
2024-09-14 17:42:20 +0200

    1.20.1

--- src/main/java/com/goby56/buildershammer/data/ModRecipeGenerator.java
+++ src/main/java/com/goby56/buildershammer/data/ModRecipeGenerator.java
@@ -3,11 +3,13 @@ package com.goby56.buildershammer.data;
 import com.goby56.buildershammer.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Consumer;
 
 public class ModRecipeGenerator extends FabricRecipeProvider {
     public ModRecipeGenerator(FabricDataOutput output) {
@@ -15,7 +17,7 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(RecipeExporter exporter) {
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.COPPER_HAMMER)
                 .pattern("CCC")
                 .pattern("CSC")
